e303e4b15c73386afaed229c29b1ae3e4aee830f
shojimonk <shojimonk@gmail.com>
2015-12-01 16:18:55 -0500

    Improved start-up logic
    Start-up now to correctly test for database connection before showing
    client frame. Loops until connection achieved.

--- The-Open-WEI-Client/src/openWEI/GUI_Runtime.java
+++ The-Open-WEI-Client/src/openWEI/GUI_Runtime.java
@@ -15,31 +15,41 @@ public class GUI_Runtime {
 		myFrame.setSize(1024, 768);
 		
 		Boolean isHost = false; 
+		String hostPort = "";
+		Boolean argsCheck = false;
+		try{
+			if(args.length == 2){
+				argsCheck = true;
+				hostPort = hostPort.concat(args[0]);
+				hostPort = hostPort.concat(args[1]);
+			}
+		}
+		catch(NullPointerException ex){
+			System.out.println("null pointer error...");
+			return;
+		}
 		
 		while(!isHost)
 		{
-			String hostPort = "";
-			if(args.length != 2)
+			
+			if(!argsCheck)
 			{
-				String host = JOptionPane.showInputDialog(null, "Enter Host", "TITLE", JOptionPane.QUESTION_MESSAGE);
-				String port = JOptionPane.showInputDialog(null, "Enter Port", "TITLE_2", JOptionPane.QUESTION_MESSAGE);
+				String host = JOptionPane.showInputDialog(null, "Enter Host", "Enter Host", JOptionPane.QUESTION_MESSAGE);
+				String port = JOptionPane.showInputDialog(null, "Enter Port", "Enter Port", JOptionPane.QUESTION_MESSAGE);
 				hostPort = hostPort.concat(host);
 				if(port.length() > 0){
 					hostPort = hostPort.concat(":");
 					hostPort = hostPort.concat(port);					
 				}
 			}
-			else
-			{
-				hostPort = hostPort.concat(args[0]);
-				hostPort = hostPort.concat(args[1]);
-			}
 			
 			isHost = myFrame.dbCheck(hostPort);			
 			if(!isHost)
 			{
-				JOptionPane.showMessageDialog(null, "Could not Contact DB at: " + hostPort, "Title_3", JOptionPane.WARNING_MESSAGE);
+				JOptionPane.showMessageDialog(null, "Could not Contact DB at: " + hostPort, "Error", JOptionPane.WARNING_MESSAGE);
+				argsCheck = false;
 				args = null;
+				hostPort = "";
 			}
 		}
 		myFrame.setVisible(true);
