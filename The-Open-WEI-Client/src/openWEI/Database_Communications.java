05a23a7ea290f774a413dd35b84f8b1ee89f5a8a
shojimonk <shojimonk@gmail.com>
2015-12-01 13:50:00 -0500

    Added PostgreSQL jdbc package and basic connectivity settings
    added postgres' .jar file to the build path. implemented
    connectionproperties building and making connection.

--- The-Open-WEI-Client/src/openWEI/Database_Communications.java
+++ The-Open-WEI-Client/src/openWEI/Database_Communications.java
@@ -1,24 +1,47 @@
 package openWEI;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.ArrayList;
+import java.sql.*;
 
 /// Class used to communicate with remote database. Utilizes JDBC API. 
 /// Builds SQL queries and commands, and parses query results before returning them to the calling object.
 public class Database_Communications {
 	
+	private Connection conn;
 	
 	/// Class constructor. Shouldn't need to do anything.
 	public Database_Communications()
 	{
-		
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("******** Error with postgres driver? *********");
+			e.printStackTrace();
+		}
 	}
 	
 	/// Checks for a database at the given host:port location.
 	/// Output: boolean representing presence of database.
-	public Boolean checkForHost(String hostPort)
+	public Boolean checkForHost(String hostPort) throws SQLException
 	{
 		
+		Properties connProperties = new Properties();
+		connProperties.setProperty("user", "username");
+		connProperties.setProperty("password", "myPass");
+		connProperties.setProperty("ssl", "true");
+		String url = "jdbc:postgresql://" +hostPort+"/ohmbaseopenwei";
+		
+		try{
+			conn = DriverManager.getConnection(url, connProperties);
+		}
+		catch(SQLException ex){
+			System.out.println("failed to connect...");
+			return false;
+		}
+		
+		//conn.
 		
 		return true;
 	}
@@ -35,12 +58,19 @@ public class Database_Communications {
 	/// Builds an SQL select statement with the given search string. Parses
 	/// results into a list of strings
 	/// output: list of strings. each entry corresponds to one row of the results.
-	public List<String> sendQuery(String searchString)
+	public ResultSet sendQuery(String searchString)
 	{
-		List<String> results = new ArrayList<String>();
+		ResultSet results = null;
 		
 		return results;
 		
 	}
 
+	public Boolean newEntry(String compType, String values)
+	{
+		
+		
+		return true;
+	}
+	
 }
