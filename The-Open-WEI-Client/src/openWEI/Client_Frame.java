caaf248f874938d558a8151c0a9f4fd9673627c6
shojimonk <shojimonk@gmail.com>
2015-12-05 23:49:05 -0500

    Implemented Modify Data functionality
    modifying existing data should now be a completed functionality.

--- The-Open-WEI-Client/src/openWEI/Client_Frame.java
+++ The-Open-WEI-Client/src/openWEI/Client_Frame.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import javax.swing.*;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.awt.event.*;
 import java.sql.*;
 
@@ -15,9 +17,10 @@ public class Client_Frame extends JFrame{
 	private Inventory_Pane iPane;
 	private Login_Pane lPane;
 	private Database_Communications comms;
+	private Client_Frame selfReference;
 	
 	private JButton createNew;
-	private JButton modifySelected;
+	private JButton commitModify;
 	private JButton deleteSelected;
 	private JButton importCSV;
 	private JButton userLogin;
@@ -31,11 +34,11 @@ public class Client_Frame extends JFrame{
 		super("The Open WEI");
 		this.setLayout(new BorderLayout());
 		iPane = new Inventory_Pane(this);
-		lPane = new Login_Pane(this);
 		comms = new Database_Communications();
+		selfReference = this;		// used for instantiating lPane from event handler.
 		
 		createNew = new JButton("Create New Entry");
-		modifySelected = new JButton("Modify Selected Data");
+		commitModify = new JButton("Commit Modifications");
 		deleteSelected = new JButton("Delete Selected Data");
 		importCSV = new JButton("Import .csv");
 		userLogin = new JButton("Log In");
@@ -47,13 +50,13 @@ public class Client_Frame extends JFrame{
 		
 		add(bottomButtons, BorderLayout.SOUTH);
 		
-		bottomButtons.add(modifySelected);
+		bottomButtons.add(commitModify);
 		bottomButtons.add(userLogin);
 		
 		actionHandler myHandler = new actionHandler();
 		userLogin.addActionListener(myHandler);
 		userLogout.addActionListener(myHandler);
-		modifySelected.addActionListener(myHandler);
+		commitModify.addActionListener(myHandler);
 		deleteSelected.addActionListener(myHandler);
 		createNew.addActionListener(myHandler);
 		importCSV.addActionListener(myHandler);
@@ -68,7 +71,11 @@ public class Client_Frame extends JFrame{
 			if(event.getSource() == userLogin)
 			{
 				remove(iPane);
+				lPane = new Login_Pane(selfReference);
 				add(lPane, BorderLayout.CENTER);
+				userLogin.setEnabled(false);
+				commitModify.setEnabled(false);
+				//lPane.requestFocusInWindow();
 				revalidate();
 				repaint();
 				lPane.setVisible(true);
@@ -77,9 +84,18 @@ public class Client_Frame extends JFrame{
 			{
 				removeAdmin();
 			}
-			else if(event.getSource() == modifySelected)
+			else if(event.getSource() == commitModify)
 			{
-				iPane.getSelectedRows();
+				List<List<String>> modifications = iPane.confirmModify();
+				if(modifications != null){
+					System.out.println("Not null.");
+					boolean updated = comms.modifyData(modifications);
+					System.out.println("updated?:" + updated);
+					return;
+				}
+				System.out.println("Is null.");
+				//iPane.getSelectedRows();
+				//iPane.createMod();
 			}
 			else if(event.getSource() == createNew)
 			{
@@ -89,10 +105,16 @@ public class Client_Frame extends JFrame{
 		}
 	}
 	
+	/*
+	 * Checks for database at given host and port. 
+	 * Output: True if successfully connected to DB, else false.
+	 */
 	public Boolean dbCheck(String hostPort)
 	{
 		try {
-			return comms.checkForHost(hostPort);
+			Boolean isHost = comms.checkForHost(hostPort);
+			if(isHost) { iPane.setTableNames(); }			// if there is a host at the port, set the list of component types 
+			return isHost;
 		} catch (SQLException ex) {
 			System.out.println("failed in client frame...");
 			ex.printStackTrace();
@@ -100,9 +122,23 @@ public class Client_Frame extends JFrame{
 		return false;
 	}
 	
-	public Boolean login(String logInfo)
+	/*
+	 * Passes user given login info to database communications.
+	 * Output: true if login name/pass match. else false.
+	 */
+	public Boolean login(String[] logInfo)
 	{
 		return comms.checkLogin(logInfo);
+	}
+	
+	public void cancelLogin()
+	{
+		remove(lPane);
+		add(iPane, BorderLayout.CENTER);
+		userLogin.setEnabled(true);
+		commitModify.setEnabled(true);
+		revalidate();
+		repaint();
 	}
 	
 	public void grantAdmin()
@@ -112,9 +148,12 @@ public class Client_Frame extends JFrame{
 		bottomButtons.add(deleteSelected);
 		bottomButtons.add(importCSV);
 		bottomButtons.add(userLogout);
+		commitModify.setEnabled(true);
 		
 		remove(lPane);
+		
 		add(iPane, BorderLayout.CENTER);
+		iPane.setAdmin(true);
 		revalidate();
 		repaint();
 	}
@@ -126,17 +165,24 @@ public class Client_Frame extends JFrame{
 		bottomButtons.remove(createNew);
 		bottomButtons.remove(deleteSelected);
 		bottomButtons.remove(importCSV);
-
+		iPane.setAdmin(false);
 		revalidate();
 		repaint();
 	}
 	
-	/// A simple method for passing user input for search requests.
-	/// output: String containing users keywords to search, and the component type selected.
-	public ResultSet search(String[] searchString)
+	/*
+	 *  A simple method for passing user input for search requests.
+	 *  output: String containing users keywords to search, and the component type selected.
+	 */
+	
+	public ResultSet search(String[] searchString)		// changed from returning ArrayList<ArrayList<String>>
 	{
-		ResultSet searchResults = comms.sendQuery(searchString);
-		return searchResults;
+		return comms.sendQuery(searchString);
 	}
 
+	public List<String> getTables()
+	{
+		return comms.getTables();
+	}
+	
 }
--- The-Open-WEI-Client/src/openWEI/Database_Communications.java
+++ The-Open-WEI-Client/src/openWEI/Database_Communications.java
@@ -3,36 +3,41 @@ package openWEI;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Properties;
-
 import jBCrypt.BCrypt;
-
 import java.sql.*;
 
-/// Class used to communicate with remote database. Utilizes JDBC API. 
-/// Builds SQL queries and commands, and parses query results before returning them to the calling object.
+/*
+ *  Class used to communicate with remote database. Utilizes JDBC API.
+ *  Builds SQL queries and commands, and parses query results before returning them to the calling object. 
+ */
 public class Database_Communications {
 	
 	private Connection conn;
 	
-	/// Class constructor. Shouldn't need to do anything.
+	/*
+	 * Class constructor. Sets database type to PostgreSQL.
+	 * Throws error if PostgreSQL driver not available.
+	 */
 	public Database_Communications()
 	{
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
-			System.out.println("******** Error with postgres driver? *********");
+			System.out.println("******** Error with postgres driver *********");
 			e.printStackTrace();
 		}
 	}
 	
-	/// Checks for a database at the given host:port location.
-	/// Output: boolean representing presence of database.
+	/*
+	 * Checks for a database at the given host:port location.
+	 * Output: boolean representing presence of database.
+	 */
 	public Boolean checkForHost(String hostPort) throws SQLException
 	{
 		
 		Properties connProperties = new Properties();
-		connProperties.setProperty("user", "Shoji");
-		connProperties.setProperty("password", "Delicious1");
+		connProperties.setProperty("user", "postgres");
+		connProperties.setProperty("password", "pandabear");
 		//connProperties.setProperty("ssl", "true");
 		String url = "jdbc:postgresql://" +hostPort+"/ohmbaseopenwei";
 		
@@ -48,13 +53,15 @@ public class Database_Communications {
 		return true;
 	}
 	
-	/// Builds query to check database for login matching the given value.
-	/// Output: boolean representing if there was a successful match.
+	/*
+	 *  Builds query to check database for login matching the given value.
+	 *  Output: boolean representing if there was a successful match.
+	 */
 	public Boolean checkLogin(String[] login)
 	{
 		ResultSet results = null;
 		Statement stmnt = null;
-		String searchable = "select * from users.account where username like '" + login[0] + "';";
+		String searchable = "select username, password from users.account where username like '" + login[0] + "';";
 		System.out.println(searchable);
 		try{
 			stmnt = conn.createStatement();
@@ -74,42 +81,26 @@ public class Database_Communications {
 		return false;
 	}
 	
-	/// Builds an SQL select statement with the given search string. Parses
-	/// results into a list of strings
-	/// output: list of strings. each entry corresponds to one row of the results.
-	public ResultSet sendQuery(String[] searchString)			// changed from returning ArrayList<ArrayList<String>>
+	/*
+	 *  Builds an SQL select statement with the given search string. Parses results into a list of strings
+	 *  output: SQL ResultSet that contains all the search results.
+	 */ 
+	public ResultSet sendQuery(String[] search)
 	{
 		ResultSet results = null;
-		Statement stmnt = null;
-		//ArrayList<ArrayList<String>> parsedResults = new ArrayList<ArrayList<String>>();
-		
-		String searchable = "select name, notes, quantity, last_modified, spec_sheets, location from " + 
-				searchString[0] + " where "+ searchString[0] + ".name like '%" + searchString[1] + "%';";
-		System.out.println(searchable);
+		//Statement stmnt = null;
+		//String searchable = "select ID, name, notes, quantity, last_modified, spec_sheets, location from " + 
+		//		searchString[0] + " where "+ searchString[0] + ".name like '%" + searchString[1] + "%';";
+		//System.out.println(searchable);
 		
 		try{
-			stmnt = conn.createStatement();
-			results = stmnt.executeQuery(searchable);
-			/*
-			while(results.next())
-			{
-				ArrayList<String> tempStore = new ArrayList<String>();
-				tempStore.add(results.getString("name"));
-				tempStore.add(results.getString("notes"));
-				
-				int intQuantity = results.getInt("quantity");
-				tempStore.add(String.format("%d", intQuantity));
-				
-				Date dateDate = results.getDate("last_modified");
-				tempStore.add(dateDate.toString());
-				
-				tempStore.add(results.getString("spec_sheets"));
-				tempStore.add(results.getString("location"));
-				
-				parsedResults.add(tempStore);
-			}
-			*/
-			//stmnt.close();
+			PreparedStatement ps = conn.prepareStatement("SELECT ID, name, notes, quantity, last_modified, spec_sheets, location from "+ search[0] +" where "+ search[0] +".name like ? ;");
+			//ps.setString(1, search[0]);
+			//ps.setString(2, search[0]);
+			ps.setString(1, "%" + search[1] + "%");
+			results = ps.executeQuery();
+			//stmnt = conn.createStatement();
+			//results = stmnt.executeQuery(searchable);
 		}
 		catch(SQLException ex){
 			System.out.println("Error with select.");
@@ -118,7 +109,42 @@ public class Database_Communications {
 		return results;
 	}
 
-	public Boolean newEntry(String compType, String values)
+	/*
+	 * 
+	 */
+	public boolean modifyData(List<List<String>> updates){
+		
+		for(List<String> each : updates){
+			try {
+				PreparedStatement ps;
+				if((each.get(1).equals("quantity")) && (!each.get(0).equals("other"))){
+					ps = conn.prepareStatement("UPDATE "+ each.get(0) +" SET "+ each.get(1) +" = ? WHERE ID = "+ each.get(3) +" ;");
+					ps.setInt(1, Integer.parseInt(each.get(2)));
+				}else{
+					ps = conn.prepareStatement("UPDATE "+ each.get(0) +" SET "+ each.get(1) +" = ? WHERE ID = "+ each.get(3) +" ;");
+					ps.setString(1, each.get(2));
+				}
+				/*
+				ps.setString(1, each.get(0));
+				ps.setString(2, each.get(1));
+				ps.setString(3, each.get(2));
+				ps.setString(4, each.get(3));
+				*/
+				System.out.println("Query is: " + ps.toString());
+				ps.executeUpdate();
+			} catch (SQLException e) {
+				System.out.println("failed to build prepared statement.");
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * For adding a new entry to the database in the chosen table.
+	 */
+	public boolean newEntry(String compType, String values)
 	{
 		
 		
@@ -130,7 +156,8 @@ public class Database_Communications {
 		List<String> columns = new ArrayList<String>();
 		try {
 			DatabaseMetaData forTables = conn.getMetaData();
-			ResultSet tableNames = forTables.getTables(null, "public", "%", null);
+			String[] types = {"TABLE"};
+			ResultSet tableNames = forTables.getTables(null, "public", "%", types);
 			while(tableNames.next()){
 				columns.add(tableNames.getString(3));
 			}
