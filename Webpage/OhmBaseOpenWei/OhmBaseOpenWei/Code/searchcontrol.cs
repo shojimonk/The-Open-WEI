658904c9f2fceefc5dbd74c991478539b8241b18
brandon1896 <brandon1896@gmail.com>
2015-12-07 00:22:39 -0500

    Commented code

--- Webpage/OhmBaseOpenWei/OhmBaseOpenWei/Code/searchcontrol.cs
+++ Webpage/OhmBaseOpenWei/OhmBaseOpenWei/Code/searchcontrol.cs
@@ -13,45 +13,55 @@
     {
         public static List<tableinfo> Read(string part,string name)
         {
-
+         // have table for all search result to enter
          List<tableinfo> list_table = new List<tableinfo>();
-         NpgsqlConnection conn = new NpgsqlConnection("Server=Localhost; Port=5432; Database=ohmbaseopenwei; User Id=postgres; Password=1234567890;");
+        // connection information for the database
+         NpgsqlConnection conn = new NpgsqlConnection("Server=Localhost; Port=5432; Database=ohmbaseopenwei; User Id=postgres; Password=pandabear;");
+         //connect to database
          conn.Open();
          NpgsqlCommand cmd;
          NpgsqlDataReader reader;
+        //cheks if the user entered any name info if not then it just returns the whole table
          if (name == "" || name == " "){
             cmd = new NpgsqlCommand("SELECT name, notes, quantity, last_modified, spec_sheets FROM " + part, conn);
            reader = cmd.ExecuteReader();
-             while (reader.Read())
+            //loads the results into the table  
+            while (reader.Read())
             {
                  list_table.Add(new tableinfo { Name = reader[0].ToString(), Notes = reader[1].ToString(), Quantity = reader[2].ToString(), Last_Modified = reader[3].ToString().Remove(reader[3].ToString().IndexOf(' ')), Spec_Sheets = reader[4].ToString() });
             }
         }
          else{
+            //first query which checks uses whole entered data as the parameters for the search
             cmd = new NpgsqlCommand("SELECT name, notes, quantity, last_modified, spec_sheets FROM "+part+" WHERE name LIKE :namep", conn);
+            //prevents injection attacks
             cmd.Parameters.Add(new NpgsqlParameter("namep", NpgsqlDbType.Varchar));
             string nameset = "%" + name + "%";
             cmd.Parameters[0].Value = nameset;
             reader = cmd.ExecuteReader();
+            //loads results into table
             while (reader.Read())
             {
                  list_table.Add(new tableinfo { Name = reader[0].ToString(), Notes = reader[1].ToString(), Quantity = reader[2].ToString(), Last_Modified = reader[3].ToString().Remove(reader[3].ToString().IndexOf(' ')), Spec_Sheets = reader[4].ToString() });
             }
             conn.Close();
             conn.Open();
+            //the entered name info is then split into mutilple strings by these delimeters
             char[] delimiterChars = { ' ', ',', '.', ':',';'};
-
             string[] split = name.Split(delimiterChars);
              List<string> splitnames =new List<string>(split);
+             //removes unwanted string values from split strings
              splitnames.Remove("");
              splitnames.Remove(null);
             string andquery = "";
             string orquery = "";
              bool firstiteration = true;
+             //new set of of strings are then put into AND and OR queries
              for (int i = 0; i < splitnames.Count; i++)
              {
                  if (firstiteration == true)
                  {
+                     //sets up the new AND and OR queries
                      andquery = andquery + ":name" + i.ToString() ;
                      orquery = orquery + ":name" + i.ToString();
                      firstiteration = false;
@@ -59,12 +69,14 @@
                  }
                  else
                  {
+                     //adds to new AND and OR queries
                      andquery = andquery +" AND name LIKE"+ " :name" + i.ToString();
                      orquery = orquery + " OR name LIKE" + " :name" + i.ToString();
                      
                  }
 
              }
+             //Searches with new AND query
              cmd = new NpgsqlCommand("SELECT name, notes, quantity, last_modified, spec_sheets FROM " + part + " WHERE name LIKE " + andquery, conn);
              for (int i = 0; i < splitnames.Count; i++)
              {
@@ -75,6 +87,7 @@
              reader = cmd.ExecuteReader();
              while (reader.Read())
              {
+                 //checks if the data that was returned was already returned by the previous query and rejects it if it was
                  bool alreadyExists = list_table.Any(x => x.Name == reader[0].ToString() && x.Notes == reader[1].ToString() && x.Quantity == reader[2].ToString() && x.Last_Modified == reader[3].ToString().Remove(reader[3].ToString().IndexOf(' ')) && x.Spec_Sheets == reader[4].ToString());
                  if (alreadyExists == false)
                  {
@@ -83,7 +96,7 @@
              }
              conn.Close();
              conn.Open();
-              
+             //Searches with new OR query 
              cmd = new NpgsqlCommand("SELECT name, notes, quantity, last_modified, spec_sheets FROM " + part + " WHERE name LIKE " + orquery, conn);
              for (int i = 0; i < splitnames.Count; i++)
              {
@@ -94,6 +107,7 @@
              reader = cmd.ExecuteReader();
              while (reader.Read())
              {
+                 //checks if the data that was returned was already returned by the previous query and rejects it if it was
                  bool alreadyExists = list_table.Any(x => x.Name == reader[0].ToString() && x.Notes == reader[1].ToString() && x.Quantity == reader[2].ToString() && x.Last_Modified == reader[3].ToString().Remove(reader[3].ToString().IndexOf(' ')) && x.Spec_Sheets == reader[4].ToString());
                  if (alreadyExists == false)
                  {
@@ -106,7 +120,7 @@
                
                    conn.Close();
                
-           
+           //returns the results of the search
             return list_table;
         }   
     }
